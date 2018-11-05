8cbfccded6b0643d9af987dd7ca8174ee8409e34
Marcos Modesto <marcosaugustomodesto15@gmail.com>
2018-11-04 22:48:49 -0200

    teste

--- src/br/com/modesto/Teste.java
+++ src/br/com/modesto/Teste.java
@@ -2,8 +2,9 @@ package br.com.modesto;
 
 public class Teste {
 	public static void main(String[] args) {
-		System.out.println("teste");
-		System.out.println("teste");
-		System.out.println("teste");
+		System.out.println("teste1");
+		System.out.println("teste2");
+		System.out.println("teste3");
+		System.out.println("teste4");
 	}
 }
