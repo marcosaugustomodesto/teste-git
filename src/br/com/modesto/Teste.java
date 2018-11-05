a6278ac13975e7b7f30792617915ad5befcd6c36
Marcos Modesto <marcosaugustomodesto15@gmail.com>
2018-11-04 22:31:58 -0200

    inicial commt

---
+++ src/br/com/modesto/Teste.java
@@ -0,0 +1,9 @@
+package br.com.modesto;
+
+public class Teste {
+	public static void main(String[] args) {
+		System.out.println("teste");
+		System.out.println("teste");
+		System.out.println("teste");
+	}
+}
